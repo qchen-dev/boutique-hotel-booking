e0023d002a7a3ccde2fd2775dd9a82cf076466bc
Nick Chen <qche.au@gmail.com>
2025-04-12 10:41:07 +1000

    chore(init): initialise project with Vite, Styled Components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 400;
+`;
+
+function App() {
+  return (
+    <>
+      <H1>Hello, the wild oasis</H1>
+    </>
+  );
+}
+
+export default App;
