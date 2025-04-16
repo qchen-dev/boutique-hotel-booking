e24a4d3ce4992bd3c6f638e8273b0beb2bafe683
Nick Chen <qche.au@gmail.com>
2025-04-16 15:29:40 +1000

    feat: add create/edit cabin form UI component

---
+++ src/features/cabins/CreateCabinForm.jsx
@@ -0,0 +1,158 @@
+import { useForm } from "react-hook-form";
+
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Textarea from "../../ui/Textarea";
+import FormRow from "../../ui/FormRow";
+
+import { useCreateCabin } from "./useCreateCabin";
+import { useEditCabin } from "./useEditCabin";
+
+function CreateCabinForm({ cabinToEdit = {} }) {
+  const { isCreating, createCabin } = useCreateCabin();
+  const { isEditing, editCabin } = useEditCabin();
+  const isWorking = isCreating || isEditing;
+
+  const { id: editId, ...editValues } = cabinToEdit;
+  const isEditSession = Boolean(editId);
+
+  const { register, handleSubmit, reset, getValues, formState } = useForm({
+    defaultValues: isEditSession ? editValues : {},
+  });
+  const { errors } = formState;
+
+  function onSubmit(data) {
+    // const image = typeof data.image === "string" ? data.image : data.image[0];
+    let image = "";
+
+    // this is the url to cloud storage after image uploading
+    if (typeof data.image === "string") image = data.image;
+    // this is the 1st upload, selecting image from local harddisk
+    else image = data.image[0];
+    // if (data.image instanceof FileList) image = data.image[0];
+    // this is after 1st time upload, during edit/update, select new image to replace
+    // if (data.image instanceof File) image = data.image[0];
+
+    if (isEditSession)
+      editCabin(
+        { newCabinData: { ...data, image }, id: editId },
+        {
+          onSuccess: (data) => {
+            reset();
+          },
+        }
+      );
+    else
+      createCabin(
+        { ...data },
+        {
+          onSuccess: (data) => {
+            reset();
+          },
+        }
+      );
+  }
+
+  function onError(errors) {
+    // console.error("Form on submit error:", errors);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow label='Cabin name' error={errors?.name?.message}>
+        <Input
+          type='text'
+          id='name'
+          disabled={isWorking}
+          {...register("name", {
+            required: "This field is required",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Maximum capacity' error={errors?.maxCapacity?.message}>
+        <Input
+          type='number'
+          id='maxCapacity'
+          disabled={isWorking}
+          {...register("maxCapacity", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Capacity should be at least 1",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Regular price' error={errors?.regularPrice?.message}>
+        <Input
+          type='number'
+          id='regularPrice'
+          disabled={isWorking}
+          {...register("regularPrice", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "The price should be at least 1",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Discount' error={errors?.discount?.message}>
+        <Input
+          type='number'
+          id='discount'
+          disabled={isWorking}
+          defaultValue={0}
+          {...register("discount", {
+            required: "This field is required",
+            validate: (value) =>
+              value <= getValues().regularPrice ||
+              "Discount should be less than regular price",
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label='Description for website'
+        error={errors?.description?.message}
+      >
+        <Textarea
+          type='number'
+          id='description'
+          defaultValue=''
+          disabled={isWorking}
+          {...register("description", {
+            required: "This field is required",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Cabin photo' error={errors?.image?.message}>
+        <FileInput
+          id='image'
+          accept='image/*'
+          {...register("image", {
+            required: !isEditSession && "This field is required",
+          })}
+        />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button $variation='secondary' type='reset'>
+          Cancel
+        </Button>
+        <Button disabled={isWorking}>
+          {isEditSession ? "Update" : "Create new cabin"}
+        </Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
